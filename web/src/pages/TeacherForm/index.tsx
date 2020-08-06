b092671e9b7182d11818de1c697676937bf4bd56
Suellyn Harumi Kohmoto <suellynhk@gmail.com>
2020-08-06 02:01:58 -0300

    Teste modificação do git

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -6,6 +6,7 @@ function TeacherForm() {
     return (
         <div id="page-teacher-form" className="container">
             <PageHeader title="Que incrível que você quer dar aulas."/>
+            
         </div>
     )
 }
